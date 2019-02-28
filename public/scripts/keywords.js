5d3ae9169252622d43647e679fd0ed9539977044
Sarah <sjmadro1@gmail.com>
2019-02-28 20:46:46 +0000

    keyword search works for testing, api catagorization funciton is not implimented. working on yelp API

--- public/scripts/keywords.js
+++ public/scripts/keywords.js
@@ -12,13 +12,48 @@ const keywords = {
   'spend', 'brand', 'merchandise']
 };
 
+words = "get new toaster".split(' ');
+for (let word of words) {
+  getCategory(word);
+}
 
-function callApi(word) {
-
+function getCategory(word) {
+  // Simple and local
   for(let category in keywords) {
     if(keywords[category].includes(word)) {
-
+      return category
     }
   }
 }
-callApi('');
+
+//   const classifiers = [
+//     { category: 'read', apiCall: amazon},
+//     { category: 'write', apiCall: amazon },
+//     { category: 'watch', apiCall: imdb },
+//     { category: 'eat', apiCall: yelp },
+//   ];
+
+//   // API oriented
+//   for(let classifier in classifiers) {
+//     // apiCall returns
+//     if (classifier.apiCall(word)) {
+//       // Do whatever we do when a category matches
+//     };
+//   }
+// }
+// getCategory('');
+
+// function callApi(category) {
+//   const read = amazon;
+//   const buy = amazon;
+//   const watch = imdbMovie;
+//   const eat = yelp;
+
+// for(let title in category) {
+//   .then
+
+// }
+
+// }
+
+//use id='new-task' for ajax call
