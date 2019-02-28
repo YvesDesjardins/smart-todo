9a7d5a534892221ccecb8c0a78a072ecdb77db1d
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-27 17:51:07 -0800

    update route as it would fail to differentiate between categories and tasks currently

--- routes/categories.js
+++ routes/categories.js
@@ -7,20 +7,13 @@ module.exports = (knex) => {
 
   // searches for users to login, currently using hardcoded 
   // users and no password check
-  router.post('/', (req, res) => {
-    knex
-      .select('*')
-      .from('users')
-      .where('email', 'test@google.com')
-      .then((results) => {
-        // on sucessful login write to cookie and redirect
-        req.session.userID = results[0].email;
-        res.redirect('/');
-      })
-      .catch((err) => {
-        throw err;
-      });
+  router.post('/:categories', (req, res) => {
+    const temp_user_id = knex('users').select('id').where('id', req.session.userID);
+    console.log(temp_user_id);
+    // knex('categories').insert()
   });
+  router.post('/:categories/edit', (req, res) => {});
+  router.post('/:categories/delete', (req, res) => {});
 
   return router;
 }
