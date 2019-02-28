ca60830d91e240361e4a128ed48d9be990565c33
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-27 17:23:10 -0800

    change name of seed files, knex runs alphabetically

--- db/seeds/step 3.js
+++ db/seeds/step 3.js
@@ -4,21 +4,21 @@ exports.seed = function (knex, Promise) {
       return Promise.all([
         knex('tasks').insert({
           id: 1,
-          name: 'Uncategorized',
-          api: 'uncat',
-          user_id: 1
+          name: 'Work on bloopityboop',
+          completed: false,
+          category_id: 1
         }),
         knex('tasks').insert({
           id: 2,
-          name: 'Movies',
-          api: 'imdb',
-          user_id: 2
+          name: 'Watch Avengers',
+          completed: false,
+          category_id: 2
         }),
         knex('tasks').insert({
           id: 3,
-          name: 'Food',
-          api: 'yelp',
-          user_id: 3
+          name: 'Eat ramen',
+          completed: false,
+          category_id: 3
         }),
       ]);
     });
