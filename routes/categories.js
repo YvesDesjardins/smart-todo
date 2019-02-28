3deae8d0b7ea91b3c81c3bf82bd795664dd02180
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-27 19:39:33 -0800

    update to use new callback function in helper getUserID()

--- routes/categories.js
+++ routes/categories.js
@@ -24,35 +24,35 @@ module.exports = (knex) => {
 
   // create new category
   router.post('/:categories', (req, res) => {
-    const temp_user_id = getUserID(knex, req.session.userID);
+    getUserID(knex, req.session.userID, (temp_user_id => {
+      knex('categories').insert({
+        name: req.params.categories,
+        api: false,
+        user_id: temp_user_id
+      })
 
-    knex('categories').insert({
-      name: req.params.categories,
-      api: false,
-      user_id: temp_user_id
-    })
-
-    res.status(200).redirect('/');
+      res.status(200).redirect('/');
+    }));
   });
   // edit existing category name
   router.post('/:categories/edit', (req, res) => {
-    const temp_user_id = getUserID(knex, req.session.userID);
-
-    knex('categories')
-      .where('user_id', temp_user_id)
-      .andWhere('name', req.params.categories)
-      .update({
-        name: req.body.name
-      });
+    getUserID(knex, req.session.userID, (temp_user_id => {
+      knex('categories')
+        .where('user_id', temp_user_id)
+        .andWhere('name', req.params.categories)
+        .update({
+          name: req.body.name
+        });
+    }));
   });
   // delete current category
   router.post('/:categories/delete', (req, res) => {
-    const temp_user_id = getUserID(knex, req.session.userID);
-
-    knex('categories')
-      .where('user_id', temp_user_id)
-      .andWhere('name', req.params.categories)
-      .del();
+    getUserID(knex, req.session.userID, (temp_user_id => {
+      knex('categories')
+        .where('user_id', temp_user_id)
+        .andWhere('name', req.params.categories)
+        .del();
+    }));
   });
 
   return router;
