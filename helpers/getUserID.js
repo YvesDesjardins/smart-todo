8f5ddeeb8354f8ee39079380e45d62f234573345
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-27 19:25:31 -0800

    add catch to chain to properly throw any errors

--- helpers/getUserID.js
+++ helpers/getUserID.js
@@ -1,10 +1,12 @@
 module.exports = (knex, uid) => {
-
   // returns user id from db
   knex('users')
     .select('*')
     .where('email', uid)
     .then((results) => {
       return results[0].id;
+    })
+    .catch((err) => {
+      throw err;
     });
 }
