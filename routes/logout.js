518991127b5140f859037ada6ef2448d8a53c691
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-27 16:32:10 -0800

    start work on POST /logout route

---
+++ routes/logout.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (knex) => {
+
+  // searches for users to login, currently using hardcoded 
+  // users and no password check
+  router.post('/', (req, res) => {
+    // knex
+    //   .select('*')
+    //   .from('users')
+    //   .where('email', 'test@google.com')
+    //   .then((results) => {
+    //     // returns the user's email if it matches one in db
+    //     res.json(results[0].email);
+    //   })
+    //   .catch((err) => {
+    //     throw err;
+    //   });
+  });
+
+  return router;
+}
