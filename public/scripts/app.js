801ae5d878c3119d6fe32d4c432c6603edc253d6
Rachel Gould <rachegould@gmail.com>
2019-02-28 03:54:17 +0000

    Testing modals

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,18 @@
+
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    // Placeholder that prints the names of all the users
-    // for(user of users) {
-    //   $("<div>").text(user.name).appendTo($("body"));
-    // }
-  });;
+  $('#example-edit-modal').click(function () {
+    console.log("click");
+    $('#edit-item-modal').modal('show');
+  });
+  
+  // Below is the starter code
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   // Placeholder that prints the names of all the users
+  //   // for(user of users) {
+  //   //   $("<div>").text(user.name).appendTo($("body"));
+  //   // }
+  // });;
 });
