4dd4e15d7f7c65cd8e4a5008523a2d8d1b66483b
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-28 11:55:20 -0800

    update tasks route to use promises rather than callbacks

--- helpers/getCategoryID.js
+++ helpers/getCategoryID.js
@@ -1,12 +1,4 @@
 module.exports = (knex, name, callback) => {
   // returns category id from db
-  knex('categories')
-    .select('*')
-    .where('name', name)
-    .then((results) => {
-      callback(results[0].id);
-    })
-    .catch((err) => {
-      throw err;
-    });
+  return knex('categories').select('*').where('name', name);
 }
