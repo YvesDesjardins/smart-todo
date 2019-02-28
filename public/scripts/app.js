0000b6c50bda5510eaa515d2bb2d6a7268711fee
Rachel Gould <rachegould@gmail.com>
2019-02-28 05:16:41 +0000

    basic styling and responsiveness complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,23 @@
 
 $(() => {
-  $('#example-edit-modal').click(function () {
+  $('#example-edit-task-modal').click(function () {
     console.log("click");
     $('#edit-item-modal').modal('show');
+  });
+
+  $('#example-edit-category-modal').click(function () {
+    console.log("click");
+    $('#edit-category-modal').modal('show');
+  });
+
+  $('#add-task').click(function () {
+    console.log("click");
+    $('#add-task-modal').modal('show');
+  });
+
+  $('#add-category').click(function () {
+    console.log("click");
+    $('#add-category-modal').modal('show');
   });
   
   // Below is the starter code
