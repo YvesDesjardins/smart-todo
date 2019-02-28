ba77988687b7d354142f4e192c63f71cee273bab
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-27 16:56:11 -0800

    add comment on ln 8

--- routes/logout.js
+++ routes/logout.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 module.exports = () => {
   router.post('/', (req, res) => {
+    // deletes user's current logged in cookie and redirect
     req.session = null;
     res.redirect('/');
   });
