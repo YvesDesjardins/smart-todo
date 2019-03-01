dbf6ab70f3242d3c198c04bd156832e1aa5b088c
Rachel Gould <rachegould@gmail.com>
2019-03-01 00:40:50 +0000

    Dashboard builds todo lists from database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,27 @@
 $(() => {
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/categories"
-  // }).done((data) => {
-  //   console.log(data);
-  // });
+
+  // Build list for a category (doesn't include items)
+  const buildList = (listObject) => {
+    const listName = listObject.name;
+    const listID = listObject.id;
+    let $cardContainer = $('<div>').addClass('card bg-light mb-3 side-scroll').attr('id', listID);
+    let $cardHeader = $('<div>').addClass('card-header category-id').text(listName);
+    let $listElement = $($cardContainer).append($($cardHeader));
+    $('#lists-container').append($listElement);
+  }
+
+  // Populate the dashboard with the user's lists and items:
+  $.ajax({
+    method: "GET",
+    url: "/categories"
+  }).done((data) => {
+    pretty = JSON.stringify(data)
+    console.log(pretty);
+    for (let list of data) {
+      console.log("list", list);
+      buildList(list);
+    }
+  });
 
   // MODALS---------------------------
   $('#example-edit-task-modal').click(function () {
