bbe09827435b86831a748b5120de2b2db2c754e2
Yves Desjardins <YvesDesjardins@gmail.com>
2019-03-01 11:49:30 -0800

    new helper functoin to return tasks that match both current user and cat

---
+++ helpers/getTasksForUser.js
@@ -0,0 +1,9 @@
+module.exports = (knex, cat_id, user_id, task_id) => {
+  return knex
+    .from('tasks')
+    .select('tasks.id')
+    .join('categories', 'tasks.category_id', '=', 'categories.id')
+    .where('tasks.category_id', cat_id)
+    .andWhere('categories.user_id', user_id)
+    .andWhere('tasks.id', task_id);
+}
