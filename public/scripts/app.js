439ed338785c5e6905fc4b73041bfd2c1cc3dd78
Sarah <sjmadro1@gmail.com>
2019-03-01 01:28:28 +0000

    Yelp API working in server.js and app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,24 @@
 $(() => {
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });;
+// 'use strict';
+// 'https://api.yelp.com/v3/businesses/search'
+
+// const apiKey = '-JNonwyUgWBSeF9CtaLXbFBV6kcc6ccx1PT - PppYbYflQGT_WjI - bnzzdT2e9HLt2cOtlp6NdslpZM_ubblVBZX9nXWrQnBuy7XRo9zsVP2L2nB7UPM7X3SzqUV4XHYx';
+
+function yelpApi() {
+  $.ajax({
+    method: 'GET',
+    url: '/api/yelp'
+  }).then((res) => {
+    console.log('res', res);
+  })
+  .catch((err) =>{
+    console.log('error', err);
+  })
+};
+console.log('work');
+yelpApi();
+
+
 
   // function createTable(table) {
   //   //take data from database and put in variables
