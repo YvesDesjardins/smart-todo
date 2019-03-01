a7e84e77742f155ce703acf1fc39bb7656ee2810
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-28 20:58:28 -0800

    fixing use of getUserID in tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,53 +8,81 @@ module.exports = (knex) => {
 
   // returns all tasks for user in current category
   router.get('/:category_id/tasks', (req, res) => {
-    knex('tasks')
-      .select('*')
-      .where('category_id', req.params.category_id)
-      .then((results) => {
-        res.json(results);
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex('tasks')
+          .select('*')
+          .where('category_id', req.params.category_id)
+          .andWhere('user_id', temp_user_id[0].id)
+          .then((results) => {
+            res.json(results);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(401).send('category has no tasks');
+          });
       })
       .catch((err) => {
-        console.log(err);
-        res.status(401).send('category has no tasks');
+        res.status(401).send('user not logged in');
       });
   });
 
   // create new task
   router.post('/:category_id/tasks/new', (req, res) => {
-    knex('tasks')
-      .insert({
-        name: req.params.task_id,
-        completed: false,
-        category_id: req.params.category_id
-      })
-      .then();
+    if (req.session.userID) {
+      knex('tasks')
+        .insert({
+          name: req.body.name,
+          completed: false,
+          category_id: req.params.category_id
+        })
+        .then();
 
-    res.status(200).redirect('/');
+      res.status(200).redirect('/');
+    } else {
+      res.status(401).send('user not logged in');
+    }
   });
   // edit current task
   router.post('/:category_id/tasks/:task_id/edit', (req, res) => {
-    knex('tasks')
-      .where('category_id', req.params.category_id)
-      .andWhere('id', req.params.task_id)
-      .update({
-        name: 'ediiittt',
-        completed: req.body.completed,
-        category_id: req.body.category_id,
-      })
-      .then();
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex
+          .from('tasks')
+          // .join('categories', 'category_id', 'categories.id')
+          .where('category_id', req.params.category_id)
+          // .andWhere('categories.user_id', temp_user_id[0].id)
+          .andWhere('id', req.params.task_id)
+          .update({
+            name: 'req.body.name',
+            completed: req.body.completed,
+            category_id: req.body.category_id,
+          })
+          .then();
 
-    res.status(200).redirect('/');
+        res.status(200).redirect('/');
+      })
+      .catch((err) => {
+        res.status(401).send('user not logged in');
+      });
   });
   // delete current task
   router.post('/:category_id/tasks/:task_id/delete', (req, res) => {
-    knex('tasks')
-      .where('category_id', req.params.category_id)
-      .andWhere('id', req.params.task_id)
-      .del()
-      .then()
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex('tasks')
+          // .join('categories', 'category_id', 'categories.id')
+          .where('category_id', req.params.category_id)
+          // .andWhere('categories.user_id', temp_user_id[0].id)
+          .andWhere('id', req.params.task_id)
+          .del()
+          .then()
 
-    res.status(200).redirect('/');
+        res.status(200).redirect('/');
+      })
+      .catch((err) => {
+        res.status(401).send('user not logged in');
+      });
   });
 
   return router;
