faa562fab0903cc62b82152c276b106b42617fcc
Rachel Gould <rachegould@gmail.com>
2019-03-01 02:17:23 +0000

    Get list items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,34 @@
 $(() => {
 
+  // Build todo tasks for a category
+
+  // AJAX call to get the todo tasks for a category
+  const getListItems = (categoryID) => {
+    $.ajax({
+      method: "GET",
+      url: `/${categoryID}/tasks`
+    }).done((data) => {
+      pretty = JSON.stringify(data);
+      console.log(pretty);
+    })
+  }
+
   // Build list for a category (doesn't include items)
   const buildList = (listObject) => {
     const listName = listObject.name;
     const listID = listObject.id;
     let $cardContainer = $('<div>').addClass('card bg-light mb-3 side-scroll').attr('id', listID);
     let $cardHeader = $('<div>').addClass('card-header category-id').text(listName);
-    let $listElement = $($cardContainer).append($($cardHeader));
+    let $listElement = $($cardContainer).prepend($($cardHeader));
     $('#lists-container').append($listElement);
   }
 
-  // Populate the dashboard with the user's lists and items:
+  // AJAX call to populate the dashboard with the user's lists and items:
   $.ajax({
     method: "GET",
     url: "/categories"
   }).done((data) => {
-    pretty = JSON.stringify(data)
+    pretty = JSON.stringify(data);
     console.log(pretty);
     for (let list of data) {
       console.log("list", list);
