565120e6b7fc28ca256ad578f9cf3c7d2c22bb95
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-28 18:04:49 -0800

    reintroduce getUserID as it is needed

--- routes/categories.js
+++ routes/categories.js
@@ -7,52 +7,76 @@ module.exports = (knex) => {
 
   // returns all current categories for user
   router.get('/', (req, res) => {
-    knex('categories')
-      .select('*')
-      .where('user_id', temp_user_id[0].id)
-      .then((results) => {
-        res.json(results);
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex('categories')
+          .select('*')
+          .where('user_id', temp_user_id[0].id)
+          .then((results) => {
+            res.json(results);
+          })
+          .catch((err) => {
+            res.status(401).send('user has no categories');
+          });
       })
       .catch((err) => {
-        res.status(401).send('user has no categories');
+        res.status(401).send('user not logged in');
       });
   });
 
   // create new category
   router.post('/:category_id', (req, res) => {
-    knex('categories')
-      .insert({
-        name: req.params.category_id,
-        api: req.body.api,
-        user_id: temp_user_id[0].id
-      })
-      .then();
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex('categories')
+          .insert({
+            name: req.params.category_id,
+            api: req.body.api,
+            user_id: temp_user_id[0].id
+          })
+          .then();
 
-    res.status(200).redirect('/');
+        res.status(200).redirect('/');
+      })
+      .catch((err) => {
+        res.status(401).send('user not logged in');
+      });
   });
   // edit existing category name
   router.post('/:category_id/edit', (req, res) => {
-    knex('categories')
-      .where('user_id', temp_user_id[0].id)
-      .andWhere('name', req.params.category_id)
-      .update({
-        name: req.body.name,
-        api: req.body.api,
-        user_id: req.body.user_id,
-      })
-      .then();
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex('categories')
+          .where('user_id', temp_user_id[0].id)
+          .andWhere('name', req.params.category_id)
+          .update({
+            name: req.body.name,
+            api: req.body.api,
+            user_id: req.body.user_id,
+          })
+          .then();
 
-    res.status(200).redirect('/');
+        res.status(200).redirect('/');
+      })
+      .catch((err) => {
+        res.status(401).send('user not logged in');
+      });
   });
   // delete current category
   router.post('/:category_id/delete', (req, res) => {
-    knex('categories')
-      .where('user_id', temp_user_id[0].id)
-      .andWhere('name', req.params.category_id)
-      .del()
-      .then();
+    getUserID(knex, req.session.userID)
+      .then((temp_user_id) => {
+        knex('categories')
+          .where('user_id', temp_user_id[0].id)
+          .andWhere('name', req.params.category_id)
+          .del()
+          .then();
 
-    res.status(200).redirect('/');
+        res.status(200).redirect('/');
+      })
+      .catch((err) => {
+        res.status(401).send('user not logged in');
+      });
   });
 
   return router;
