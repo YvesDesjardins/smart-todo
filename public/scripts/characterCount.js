12f7c2ba5a896363afd1d377bb63a3ac146c76b6
Sarah <sjmadro1@gmail.com>
2019-03-04 00:28:52 +0000

    max length set to 30 for all <input> fields. add character counter to modals.

---
+++ public/scripts/characterCount.js
@@ -0,0 +1,29 @@
+$(() => {
+
+  // $('input[type="text"]').attr({
+  //   maxLength: 30
+  // })
+
+
+  $('input[type="text"]').on(`input`, function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('input[type="text"]').attr({
+      maxLength: 30
+    })
+    let $textAreaLeng = $(this).val().length;
+    let $counter = $(this).parent().find('.counter');
+    $counter.text(30 - $textAreaLeng);
+    if ($textAreaLeng > 30) {
+      $counter.css(`color`, `red`);
+    } else {
+      $counter.css(`color`, `black`);
+    }
+  })
+
+  //if we decide to add the counter, the code below would be added to
+  //index.ejs in all the modal forms below the input tags.
+
+  /* < span class = "counter" > 30 < /span> */
+
+});
